091f8022386caf898afd7f6e90fc72b6d4b2ab22
Shaun Putnam <shaun.putnam@student.swic.edu>
2025-05-13 20:47:18 -0500

    feat: add fetch function to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,24 @@
 import Header from './components/header'
+import ShipsGrid from './components/ships-grid'
+import { useState, useEffect } from 'react'
 
 export default function App() {
+  const [ships, setShips] = useState([])
+
+  useEffect(() => {
+    async function fetchShips() {
+      const response = await fetch('https://swapi.dev/api/starships/')
+      const data = await response.json()
+      setShips(data.results)
+    }
+    fetchShips()
+  }, [])
+
   return (
-    <main>
-      <Header />s
+    <main className="container mx-auto p-4">
+      <Header />
+      <h1 className="text-2xl font-bold">Star Wars Ships</h1>
+      <ShipsGrid ships={ships} />
     </main>
   )
 }

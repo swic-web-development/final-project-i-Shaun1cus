06fd29c03df471cb885616184d63df86a0eae5b6
Shaun Putnam <shaun.putnam@student.swic.edu>
2025-05-13 20:40:22 -0500

    feat: add code to render card

---
+++ src/components/ships-grid/index.jsx
@@ -0,0 +1,11 @@
+import Card from './card'
+
+export default function ShipsGrid({ ships }) {
+  return (
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      {ships.map((ship) => (
+        <Card ship={ship} key={ship.name} />
+      ))}
+    </div>
+  )
+}

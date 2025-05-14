e6816c848f16fdd06537cca57a37c9d38404a733
Shaun Putnam <shaun.putnam@student.swic.edu>
2025-05-14 06:42:57 -0500

    feat: re-fix fetch function

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,22 @@
 import Header from './components/header'
+import ShipsGrid from './components/ships-grid'
+import { useState, useEffect } from 'react'
 
 export default function App() {
+  const [ships, setShips] = useState([])
+
+  useEffect(() => {
+    fetch('https://swapi.tech/api/starships/')
+      .then((response) => response.json())
+      .then((data) => {
+        setShips(data.results)
+      })
+      .catch((error) => console.error('Error fetching data:', error))
+  }, [])
   return (
     <main>
       <Header />
+      <ShipsGrid ships={ships} />
     </main>
   )
 }
--- src/components/ships-grid/index.jsx
+++ src/components/ships-grid/index.jsx
@@ -4,7 +4,7 @@ export default function ShipsGrid({ ships }) {
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
       {ships.map((ship) => (
-        <Card ship={ship} key={ship.name} />
+        <Card ship={ship} key={ship.uid} />
       ))}
     </div>
   )

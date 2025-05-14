3759c47c16aaab8f4d5e3a7d3e5e1e8c99600a7c
Shaun Putnam <shaun.putnam@student.swic.edu>
2025-05-14 12:41:42 -0500

    style: update header styles

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header>
-      <h1 className="bg-gradient-to-r from-blue-500 to-red-600 p-4 text-center text-3xl text-white">
+      <h1 className="bg-gradient-to-r from-blue-500 to-red-600 p-4 text-center text-3xl font-bold text-white">
         Star Wars Battleships!
       </h1>
     </header>
